b8f463cc9d75d9ecd6e3909faf49edd8e28168ca
Andy Durette <duretteandy@gmail.com>
2020-04-04 13:22:52 -0400

    New layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {increment} from './actions';
-import {decrement} from './actions';
-
+import { Provider } from 'react-redux';
+import store from './redux/store'
+import './App.css';
+import CakeContainer from './components/CakeContainer'
 
 function App() {
-  const counter = useSelector( state => state.counter);
-  const dispatch = useDispatch();
   return (
     <div className="App">
-    <h1>Counter {counter}</h1>
-    <button onClick={() => dispatch(increment())}>+</button>
-    <button onClick={() => dispatch(decrement())}>-</button>
+      <Provider store={store}>
+        <CakeContainer></CakeContainer>
+      </Provider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
-import allReducer from './reducers';
-import { Provider } from 'react-redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
+// import allReducer from './reducers';
 
-const store = createStore(
-  allReducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const store = createStore(
+//   allReducer, //applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 
 
-ReactDOM.render(<React.StrictMode><Provider store={store}><App /></Provider></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
