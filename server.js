597ef16c05c404493c408cdf06b276b7741b69bc
Andy Durette <duretteandy@gmail.com>
2020-05-26 13:39:26 -0400

    correcting server directory call

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(favicon(__dirname + '/client/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 app.listen(port);
