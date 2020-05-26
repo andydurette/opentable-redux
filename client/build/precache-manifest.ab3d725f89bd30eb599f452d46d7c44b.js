37701e67269b27fae4c659e8130da87b557e2d40
Andy Durette <duretteandy@gmail.com>
2020-05-26 13:44:10 -0400

    Git ignore was set to ignore build folder needed to update

---
+++ client/build/precache-manifest.ab3d725f89bd30eb599f452d46d7c44b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b3c8fa2753a92f32dd4bc0c69d0436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d38ee792d9bb4dcd6e1",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5820853b3c2c642195a5",
+    "url": "/static/css/main.be860033.chunk.css"
+  },
+  {
+    "revision": "1d38ee792d9bb4dcd6e1",
+    "url": "/static/js/2.268491d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.268491d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5820853b3c2c642195a5",
+    "url": "/static/js/main.806a665c.chunk.js"
+  },
+  {
+    "revision": "4968badfbbf7744550b2",
+    "url": "/static/js/runtime-main.be1ad068.js"
+  }
+]);
